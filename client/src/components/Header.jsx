af459e9657c12163f75b5b54dc5bb9cb602c4fec
Frank-Colorado <chawnkiee@gmail.com>
2024-01-21 17:10:34 -0500

    changed styling in Header component

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,11 +1,18 @@
 import { Box, Typography, AppBar, Toolbar } from '@mui/material';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar sx={{ height: '6.5rem' }}>
-          <Typography align="center" variant="h2">
+          <Typography
+            component={Link}
+            to="/"
+            align="center"
+            variant="h2"
+            sx={{ fontWeight: 'bold', textDecoration: 'none', color: 'white' }}
+          >
             Fresh n Stuff
           </Typography>
         </Toolbar>
