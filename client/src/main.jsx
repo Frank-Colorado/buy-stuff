a9098a3ba9499fec2c2dc75c496cbfa5e6f9afa1
Frank-Colorado <chawnkiee@gmail.com>
2024-01-21 16:46:48 -0500

    added ThemeProvider to main.jsx file

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,5 +1,11 @@
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
+import { ThemeProvider } from '@mui/material/styles';
+import { theme } from './styles';
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <ThemeProvider theme={theme}>
+    <App />
+  </ThemeProvider>
+);
