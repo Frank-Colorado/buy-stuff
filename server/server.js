62654d25854587d589f18681db423d2c5a4ff875
Frank-Colorado <chawnkiee@gmail.com>
2024-01-22 14:21:13 -0500

    set up server in server.js file

---
+++ server/server.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
+import dotenv from 'dotenv';
+dotenv.config();
+
+const main = async () => {
+  const PORT = process.env.PORT || 3001;
+
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+  });
+
+  const app = express();
+
+  app.use(express.urlencoded({ extended: false }));
+  app.use(express.json());
+
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+  }
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  });
+
+  await server.start();
+  server.applyMiddleware({ app });
+
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}!`);
+      console.log(
+        `Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`
+      );
+    });
+  });
+};
+
+main().catch((err) => {
+  console.error(err);
+});
