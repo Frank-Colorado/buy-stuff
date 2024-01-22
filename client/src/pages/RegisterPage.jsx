02beac81e44a525da49127390efa9076a06aeefd
Frank-Colorado <chawnkiee@gmail.com>
2024-01-22 16:25:31 -0500

    added content to RegisterPage

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,10 +1,47 @@
+import { Box, Typography, Button } from '@mui/material';
+
 import SignupForm from '../components/forms/SignupForm';
 
 const RegisterPage = () => {
   return (
-    <div>
-      <SignupForm />
-    </div>
+    <Box
+      sx={{
+        display: 'flex',
+        alignItems: 'center',
+        flexDirection: 'column',
+      }}
+    >
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          width: '100%',
+          maxWidth: '40.0rem',
+          margin: '4rem auto',
+          border: '1px solid black',
+          padding: '1rem',
+        }}
+      >
+        <Typography
+          textTransform="uppercase"
+          sx={{
+            fontSize: '2.2rem',
+            fontWeight: 'bold',
+            lineHeight: '1',
+          }}
+        >
+          Register
+        </Typography>
+        <SignupForm />
+      </Box>
+      <Typography>
+        Already have an account?
+        <Button color="secondary" href="/login">
+          Login
+        </Button>
+      </Typography>
+    </Box>
   );
 };
 
