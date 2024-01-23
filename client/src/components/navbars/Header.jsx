bb2711900452bdba1be19daebd8e1500672740d8
Frank-Colorado <chawnkiee@gmail.com>
2024-01-22 19:52:50 -0500

    added auth and logout button to header navbar

--- client/src/components/navbars/Header.jsx
+++ client/src/components/navbars/Header.jsx
@@ -1,7 +1,10 @@
 import { Box, Typography, AppBar, Toolbar, Button } from '@mui/material';
 import { Link } from 'react-router-dom';
+import Auth from '../../utils/auth';
 
 const Header = () => {
+  const isLoggedIn = Auth.loggedIn();
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static" color="secondary">
@@ -17,32 +20,49 @@ const Header = () => {
           </Typography>
           <Box sx={{ flexGrow: 1 }} />
           <Box sx={{ display: { xs: 'none', md: 'flex' }, mr: 7 }}>
-            <Button
-              component={Link}
-              to="/register"
-              variant="text"
-              sx={{
-                mr: 2,
-                '&:hover': {
-                  backgroundColor: '#000000',
-                  color: 'white',
-                },
-              }}
-            >
-              Register
-            </Button>
-            <Button
-              component={Link}
-              to="/login"
-              variant="text"
-              sx={{
-                '&:hover': {
-                  borderBottom: '2px solid #000000',
-                },
-              }}
-            >
-              Login
-            </Button>
+            {isLoggedIn ? (
+              <Button
+                onClick={Auth.logout}
+                variant="text"
+                sx={{
+                  mr: 2,
+                  '&:hover': {
+                    borderBottom: '2px solid #000000',
+                  },
+                }}
+              >
+                Logout
+              </Button>
+            ) : (
+              <>
+                <Button
+                  component={Link}
+                  to="/register"
+                  variant="text"
+                  sx={{
+                    mr: 2,
+                    '&:hover': {
+                      backgroundColor: '#000000',
+                      color: 'white',
+                    },
+                  }}
+                >
+                  Register
+                </Button>
+                <Button
+                  component={Link}
+                  to="/login"
+                  variant="text"
+                  sx={{
+                    '&:hover': {
+                      borderBottom: '2px solid #000000',
+                    },
+                  }}
+                >
+                  Login
+                </Button>
+              </>
+            )}
           </Box>
         </Toolbar>
       </AppBar>
