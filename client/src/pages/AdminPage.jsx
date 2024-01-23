fcb5cc258079adaf5a938ad0762957786547a2ca
Frank-Colorado <chawnkiee@gmail.com>
2024-01-23 18:29:55 -0500

    added content to AdminPage

--- client/src/pages/AdminPage.jsx
+++ client/src/pages/AdminPage.jsx
@@ -1,3 +1,8 @@
+// components for admin page
+import UserSidebar from '../components/navbars/UserSidebar.jsx';
+import ProductList from '../components/admin/ProductList.jsx';
+import { Grid, Box, Button, Typography } from '@mui/material';
+
 import Auth from '../utils/auth.js';
 
 const AdminPage = () => {
@@ -9,9 +14,22 @@ const AdminPage = () => {
   }
 
   return (
-    <div>
-      <h1>Admin Page</h1>
-    </div>
+    <Grid container sx={{ height: '100%  ' }}>
+      <Grid item xs={3}>
+        <UserSidebar />
+      </Grid>
+      <Grid item xs={9}>
+        <Box>
+          <Button variant="contained" color="primary" sx={{ mt: 5 }}>
+            Add New Product
+          </Button>
+        </Box>
+        <Typography variant="h2" textAlign="center" sx={{ mt: 5 }}>
+          Manage Products
+        </Typography>
+        <ProductList />
+      </Grid>
+    </Grid>
   );
 };
 
