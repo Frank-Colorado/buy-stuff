eb9e81c49adcfe1a0baf1ec6db2dd4bb0b4b9343
Frank-Colorado <chawnkiee@gmail.com>
2024-01-23 17:46:43 -0500

    changes to all queries/mutations in resolver.js file

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -6,9 +6,14 @@ const resolvers = {
   Query: {
     me: async (_root, _args, context) => {
       if (context.user) {
-        const userData = await User.findById(context.user._id);
+        try {
+          const userData = await User.findById(context.user._id);
 
-        return userData;
+          return userData;
+        } catch (err) {
+          console.log('Error getting user data', err);
+          throw new AuthenticationError('Failed to get user data');
+        }
       }
       throw new AuthenticationError('Not logged in');
     },
@@ -32,42 +37,97 @@ const resolvers = {
     },
   },
   Mutation: {
-    // create User mutation
+    // Mutation for creating a new user
     createUser: async (_root, { username, email, password }) => {
-      const user = await User.create({ username, email, password });
-      const token = signToken(user);
-      return { token, user };
+      try {
+        // create a new user
+        const user = await User.create({ username, email, password });
+        // create a token with the new user info
+        const token = signToken(user);
+        // return the token and user as an Auth object
+        return { token, user };
+      } catch (err) {
+        console.log('Error creating user', err);
+        throw new AuthenticationError('Failed to create user');
+      }
     },
     // login mutation
     login: async (_root, { email, password }) => {
-      const user = await User.findOne({ email });
-      if (!user) {
-        throw new AuthenticationError('Incorrect credentials');
+      try {
+        // find the user by email
+        const user = await User.findOne({ email });
+        // if the user is not found, throw an error
+        if (!user) {
+          throw new AuthenticationError('Incorrect credentials');
+        }
+        // check if the password is correct
+        const correctPw = await user.isCorrectPassword(password);
+        // if the password is not correct, throw an error
+        if (!correctPw) {
+          throw new AuthenticationError('Incorrect credentials');
+        }
+        // if email and password are correct, create a token with the user info
+        const token = signToken(user);
+        // return the token and user as an Auth object
+        return { token, user };
+      } catch (err) {
+        console.log('Error logging in', err);
+        throw new AuthenticationError('Failed to log in');
       }
-
-      const correctPw = await user.isCorrectPassword(password);
-
-      if (!correctPw) {
-        throw new AuthenticationError('Incorrect credentials');
-      }
-
-      const token = signToken(user);
-      return { token, user };
     },
   },
   // Mutation for adding a clothing item
   addClothing: async (_root, args, context) => {
     // if the context has a 'user' property that contains the 'admin' role in its 'roles' array
     // then allow the mutation to execute
-    // This will be commented out for now to not interfere with the front end
     // if (context.user.roles.includes('admin')) {
-    //   const clothing = await Clothing.create({ ...args });
-    //   return clothing;
+    try {
+      const clothing = await Clothing.create({ ...args });
+      return clothing;
+    } catch (err) {
+      console.log('Error adding clothing', err);
+      throw new Error('Failed to create clothing item');
+    }
     // }
     // // if the user is not an admin, throw an error
     // throw new AuthenticationError('Not authorized');
-    const clothing = await Clothing.create({ ...args });
-    return clothing;
+  },
+  // Mutation for updating a specific clothing item using its _id
+  updateClothing: async (_root, args, context) => {
+    const { clothingId } = args;
+    // if the context has a 'user' property that contains the 'admin' role in its 'roles' array
+    // then allow the mutation to execute
+    // This will be commented out for now to not interfere with the front end
+    // if (context.user.roles.includes('admin')) {
+    try {
+      const clothing = await Clothing.findByIdAndUpdate(clothingId, {
+        ...args,
+      });
+      return clothing;
+    } catch (err) {
+      console.log('Error updating clothing', err);
+      throw new Error('Failed to update clothing item');
+    }
+    // }
+    // // if the user is not an admin, throw an error
+    // throw new AuthenticationError('Not authorized');
+  },
+  // Mutation for deleting a specific clothing item using its _id
+  deleteClothing: async (_root, { clothingId }, context) => {
+    // if the context has a 'user' property that contains the 'admin' role in its 'roles' array
+    // then allow the mutation to execute
+    // This will be commented out for now to not interfere with the front end
+    // if (context.user.roles.includes('admin')) {
+    try {
+      const clothing = await Clothing.findByIdAndDelete(clothingId);
+      return clothing;
+    } catch (err) {
+      console.log('Error deleting clothing', err);
+      throw new Error('Failed to delete clothing item');
+    }
+    // }
+    // // if the user is not an admin, throw an error
+    // throw new AuthenticationError('Not authorized');
   },
 };
 
