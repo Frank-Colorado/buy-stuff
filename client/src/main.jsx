1f81313ce72d3f3d500a995c892ed40db498c7a5
Frank-Colorado <chawnkiee@gmail.com>
2024-01-22 19:23:27 -0500

    ApolloClient setup in main.jsx file

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,48 @@
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
+
+// Apollo client
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+//styling
 import './index.css';
 import { ThemeProvider } from '@mui/material/styles';
 import { theme } from './styles';
 
+// Apollo client setup
+const isDev = import.meta.env.DEV;
+
+const graphqlUri = isDev ? 'http://localhost:3001/graphql' : '/graphql';
+
+const httpLink = createHttpLink({
+  uri: graphqlUri,
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('jwtToken');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <ThemeProvider theme={theme}>
-    <App />
-  </ThemeProvider>
+  <ApolloProvider client={client}>
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </ApolloProvider>
 );
