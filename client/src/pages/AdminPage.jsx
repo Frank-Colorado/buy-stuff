f4c10772b22a29f799fe8ea1cc4aa3f9f3191016
Frank-Colorado <chawnkiee@gmail.com>
2024-01-24 19:11:36 -0500

    added content to AdminPage.jsx

--- client/src/pages/AdminPage.jsx
+++ client/src/pages/AdminPage.jsx
@@ -2,9 +2,17 @@
 import AdminSidebar from '../components/admin/AdminSidebar.jsx';
 import ManageProducts from '../components/admin/ManageProducts.jsx';
 import CreateProduct from '../components/admin/CreateProduct.jsx';
-
-import { Grid, Box, Button, Typography } from '@mui/material';
-
+// React hooks
+import { useEffect } from 'react';
+// Redux hooks
+import { useDispatch } from 'react-redux';
+import { setProducts } from '../store/index.js';
+// GraphQL hooks
+import { useQuery } from '@apollo/client';
+import { QUERY_ALL_PRODUCTS } from '../graphQL/queries.js';
+// MUI components
+import { Grid, Box, CircularProgress } from '@mui/material';
+// Utils
 import Auth from '../utils/auth.js';
 
 const AdminPage = () => {
@@ -14,6 +22,17 @@ const AdminPage = () => {
   if (!isAdmin) {
     window.location.replace('/');
   }
+
+  // Setting up the hooks
+  const dispatch = useDispatch();
+  const { loading, data } = useQuery(QUERY_ALL_PRODUCTS);
+
+  // If data exists, dispatch the data to the store
+  useEffect(() => {
+    if (data) {
+      dispatch(setProducts(data.allClothing));
+    }
+  }, [data, dispatch]);
 
   return (
     <Grid
@@ -26,8 +45,23 @@ const AdminPage = () => {
         <AdminSidebar />
       </Grid>
       <Grid item xs={10}>
-        <CreateProduct />
-        <ManageProducts />
+        {loading ? (
+          <Box
+            sx={{
+              height: '100%',
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+            }}
+          >
+            <CircularProgress />
+          </Box>
+        ) : (
+          <>
+            <CreateProduct />
+            <ManageProducts />
+          </>
+        )}
       </Grid>
     </Grid>
   );
