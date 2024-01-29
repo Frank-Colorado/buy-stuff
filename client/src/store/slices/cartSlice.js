242128462bd3db52f520a07e4e8f3e14ec41ed20
Frank-Colorado <chawnkiee@gmail.com>
2024-01-29 13:33:48 -0500

    created cartSlice with reducers

---
+++ client/src/store/slices/cartSlice.js
@@ -0,0 +1,23 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const cartSlice = createSlice({
+  name: 'cart',
+  initialState: [],
+  reducers: {
+    addItem: (state, { payload }) => {
+      // Add the new item to the end of the array
+      state.push(payload);
+    },
+    removeItem: (state, { payload }) => {
+      // Remove the item from state
+      return state.filter((item) => item._id !== payload);
+    },
+    clearCart: (_state, _action) => {
+      // Reset the cart to an empty array
+      return [];
+    },
+  },
+});
+
+export const { addItem, removeItem, clearCart } = cartSlice.actions;
+export const cartReducer = cartSlice.reducer;
