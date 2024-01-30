66b160825a9c26858eb93641fa0490a43c393039
Frank-Colorado <chawnkiee@gmail.com>
2024-01-30 15:36:21 -0500

    small styling and content changes in Checkout components

--- client/src/pages/CheckoutPage.jsx
+++ client/src/pages/CheckoutPage.jsx
@@ -1,11 +1,22 @@
 // My components
 import CheckoutDetails from '../components/checkout/CheckoutDetails.jsx';
+// MUI components
+import { Grid } from '@mui/material';
 
 const CheckoutPage = () => {
   return (
-    <div>
+    <Grid
+      container
+      direction="row"
+      justifyContent="center"
+      alignItems="center"
+      spacing={2}
+      sx={{
+        minHeight: 'calc(100vh - 10.5rem)',
+      }}
+    >
       <CheckoutDetails />
-    </div>
+    </Grid>
   );
 };
 
