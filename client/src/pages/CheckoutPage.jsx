6e1b6ffce05b74a30d31bd7e0ed0d4317ee97ba3
Frank-Colorado <chawnkiee@gmail.com>
2024-02-04 17:30:13 -0500

    added page redirect in CheckoutPage if user's cart is empty

--- client/src/pages/CheckoutPage.jsx
+++ client/src/pages/CheckoutPage.jsx
@@ -4,6 +4,13 @@ import CheckoutDetails from '../components/checkout/CheckoutDetails.jsx';
 import { Grid } from '@mui/material';
 
 const CheckoutPage = () => {
+  // Get the user's cart from localStorage
+  const cart = JSON.parse(localStorage.getItem('userCart'));
+  // If the cart is empty, redirect to the home page
+  if (!cart || cart.length === 0) {
+    window.location.replace('/');
+  }
+
   return (
     <Grid
       container
